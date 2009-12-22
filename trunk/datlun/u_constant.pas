3616582fdd3561ce392356299c71e37c6613a6d2
pchev <pchev@c546a64b-cd0d-0410-a96a-e873a23ec1ac>
2009-12-22 10:09:47 +0000

    do not activate bump on second copy (out of memory error) change version date

--- trunk/datlun/u_constant.pas
+++ trunk/datlun/u_constant.pas
@@ -30,7 +30,7 @@ uses
 
 const crlf = chr(10)+chr(13);
       AVLversion = '5.0';
-      Splashversion = AVLversion+' 2009-12-21';
+      Splashversion = AVLversion+' 2009-12-22';
       VersionName = 'Pro';
       avlcpy = 'Copyright (C) 2002-2009 Christian Legrand, Patrick Chevalley';
       vmaurl='http://ap-i.net/avl';
