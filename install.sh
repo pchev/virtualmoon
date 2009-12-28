cda372e5fc225c5728d81bd6325e8fe5dc7a4a3a
Patrick Chevalley <pch@ap-i.net>
2009-12-28 22:01:36 +0000

    build scripts

--- install.sh
+++ install.sh
@@ -16,13 +16,32 @@ install -m 755 -d $destdir/share/applications
 install -m 755 -d $destdir/share/doc
 install -m 755 -d $destdir/share/doc/virtualmoon
 install -m 755 -d $destdir/share/pixmaps
+install -m 755 -d $destdir/share/virtualmoon
+install -m 755 -d $destdir/share/virtualmoon/data
+install -m 755 -d $destdir/share/virtualmoon/data/jpleph
+install -m 755 -d $destdir/share/virtualmoon/language
+install -m 755 -d "$destdir/share/virtualmoon/My Images"
+install -m 755 -d $destdir/share/virtualmoon/Database
+install -m 755 -d $destdir/share/virtualmoon/doc
+install -m 755 -d $destdir/share/virtualmoon/Encyclopedia
+install -m 755 -d $destdir/share/virtualmoon/Textures
 
 install -v -m 755 -s virtualmoon/atlun  $destdir/bin/atlun
 install -v -m 755 -s datlun/datlun  $destdir/bin/datlun
 install -v -m 755 -s photlun/photlun  $destdir/bin/photlun
 install -v -m 644 -s virtualmoon/library/plan404/libplan404.so  $destdir/lib/libplan404.so
+install -v -m 644 virtualmoon/language/maplun.en.po $destdir/share/virtualmoon/language/
+install -v -m 644 virtualmoon/language/maplun.fr.po $destdir/share/virtualmoon/language/
+install -v -m 644 datlun/language/datlun.en.po $destdir/share/virtualmoon/language/
+install -v -m 644 datlun/language/datlun.fr.po $destdir/share/virtualmoon/language/
+install -v -m 644 datlun/language/vmadatabase.en.po $destdir/share/virtualmoon/language/
+install -v -m 644 datlun/language/vmadatabase.fr.po $destdir/share/virtualmoon/language/
+install -v -m 644 photlun/language/photlun.en.po $destdir/share/virtualmoon/language/
+install -v -m 644 photlun/language/photlun.fr.po $destdir/share/virtualmoon/language/
 install -v -m 644 Installer/Linux/vmapro/share/applications/virtualmoon.desktop $destdir/share/applications/virtualmoon.desktop
 install -v -m 644 Installer/Linux/vmapro/share/doc/virtualmoon/changelog $destdir/share/doc/virtualmoon/changelog
 install -v -m 644 Installer/Linux/vmapro/share/doc/virtualmoon/copyright $destdir/share/doc/virtualmoon/copyright
 install -v -m 644 Installer/Linux/vmapro/share/pixmaps/virtualmoon.xpm $destdir/share/pixmaps/virtualmoon.xpm
-
+install -v -m 644 data/country.tab $destdir/share/virtualmoon/data/
+install -v -m 644 data/retic.cur $destdir/share/virtualmoon/data/
+install -v -m 644 data/jpleph/unxp1900.421 $destdir/share/virtualmoon/data/jpleph
