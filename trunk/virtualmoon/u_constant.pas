c55b05533b3e69c83a1da1488d5b0776c5915d32
pchev <pchev@c546a64b-cd0d-0410-a96a-e873a23ec1ac>
2009-12-29 14:57:40 +0000

    change version to 5.1 beta

--- trunk/virtualmoon/u_constant.pas
+++ trunk/virtualmoon/u_constant.pas
@@ -33,8 +33,8 @@ uses
 
 const crlf = chr(10)+chr(13);
       cpyr = chr($a9)+chr($c2);  // ©
-      AVLversion = '5.0';
-      Splashversion = AVLversion+' 2009-12-22';
+      AVLversion = '5.1 beta';
+      Splashversion = AVLversion+' 2009-12-29';
       VersionName = 'AtLun';
       avlcpy = 'Copyright '+cpyr+' 2002-2009 Christian Legrand, Patrick Chevalley';
       vmaurl='http://ap-i.net/avl';
