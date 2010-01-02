7d0bbaefb426b8de1300cbc47ff51f815014c328
pchev <pchev@c546a64b-cd0d-0410-a96a-e873a23ec1ac>
2010-01-02 09:57:17 +0000

    Mac OS X installer

--- trunk/uninstall_darwin.sh
+++ trunk/uninstall_darwin.sh
@@ -16,3 +16,32 @@ rm -fv $destdir/libplan404.dylib
 rm -rf $destdir/atlun.app 
 rm -rf $destdir/datlun.app
 rm -rf $destdir/photlun.app
+
+rm -fv $destdir/licence.txt
+rm -fv $destdir/readme.txt
+rm -fv $destdir/Textures/Overlay/caption/*.jpg
+rm -fv $destdir/Textures/Overlay/*.jpg
+rm -fv $destdir/Textures/Bumpmap/*
+rm -fv $destdir/Textures/Airbrush/L1/*.jpg
+rm -fv $destdir/Encyclopedia/*
+rm -fv $destdir/doc/*
+rm -fv $destdir/Database/*
+rm -fv $destdir/language/*.po
+rm -fv $destdir/data/jpleph/*
+rm -fv $destdir/data/*.cur
+rm -fv $destdir/data/*.tab
+
+rmdir  $destdir/Textures/Overlay/caption
+rmdir  $destdir/Textures/Overlay
+rmdir  $destdir/Textures/Bumpmap
+rmdir  $destdir/Textures/Airbrush/L1
+rmdir  $destdir/Textures/Airbrush
+rmdir  $destdir/Encyclopedia
+rmdir  $destdir/doc
+rmdir  $destdir/Database
+rmdir  $destdir/language
+rmdir  $destdir/data/jpleph
+rmdir  $destdir/data
+rmdir  $destdir/Textures
+rmdir  "$destdir/My Images"
+rmdir  $destdir
