5234bc3d1c4be9f0b6be7107da84375d163ad9b0
pchev <pchev@c546a64b-cd0d-0410-a96a-e873a23ec1ac>
2010-01-17 14:17:45 +0000

    add build procedure for rpm, deb, windows

--- trunk/install.sh
+++ trunk/install.sh
@@ -16,6 +16,15 @@ install -m 755 -d $destdir/share/applications
 install -m 755 -d $destdir/share/doc
 install -m 755 -d $destdir/share/doc/virtualmoon
 install -m 755 -d $destdir/share/pixmaps
+install -v -m 755 -s virtualmoon/atlun  $destdir/bin/atlun
+install -v -m 755 -s datlun/datlun  $destdir/bin/datlun
+install -v -m 755 -s photlun/photlun  $destdir/bin/photlun
+install -v -m 644 -s virtualmoon/library/plan404/libplan404.so  $destdir/lib/libplan404.so
+install -v -m 644 Installer/Linux/vmapro/share/applications/virtualmoon.desktop $destdir/share/applications/virtualmoon.desktop
+install -v -m 644 Installer/Linux/vmapro/share/doc/virtualmoon/changelog $destdir/share/doc/virtualmoon/changelog
+install -v -m 644 Installer/Linux/vmapro/share/doc/virtualmoon/copyright $destdir/share/doc/virtualmoon/copyright
+install -v -m 644 Installer/Linux/vmapro/share/pixmaps/virtualmoon.xpm $destdir/share/pixmaps/virtualmoon.xpm
+
 install -m 755 -d $destdir/share/virtualmoon
 install -m 755 -d $destdir/share/virtualmoon/data
 install -m 755 -d $destdir/share/virtualmoon/data/jpleph
@@ -30,11 +39,6 @@ install -m 755 -d $destdir/share/virtualmoon/Textures/Airbrush/L1
 install -m 755 -d $destdir/share/virtualmoon/Textures/Bumpmap
 install -m 755 -d $destdir/share/virtualmoon/Textures/Overlay
 install -m 755 -d $destdir/share/virtualmoon/Textures/Overlay/caption
-
-install -v -m 755 -s virtualmoon/atlun  $destdir/bin/atlun
-install -v -m 755 -s datlun/datlun  $destdir/bin/datlun
-install -v -m 755 -s photlun/photlun  $destdir/bin/photlun
-install -v -m 644 -s virtualmoon/library/plan404/libplan404.so  $destdir/lib/libplan404.so
 install -v -m 644 virtualmoon/language/maplun.en.po $destdir/share/virtualmoon/language/
 install -v -m 644 virtualmoon/language/maplun.fr.po $destdir/share/virtualmoon/language/
 install -v -m 644 datlun/language/datlun.en.po $destdir/share/virtualmoon/language/
@@ -43,10 +47,6 @@ install -v -m 644 datlun/language/vmadatabase.en.po $destdir/share/virtualmoon/l
 install -v -m 644 datlun/language/vmadatabase.fr.po $destdir/share/virtualmoon/language/
 install -v -m 644 photlun/language/photlun.en.po $destdir/share/virtualmoon/language/
 install -v -m 644 photlun/language/photlun.fr.po $destdir/share/virtualmoon/language/
-install -v -m 644 Installer/Linux/vmapro/share/applications/virtualmoon.desktop $destdir/share/applications/virtualmoon.desktop
-install -v -m 644 Installer/Linux/vmapro/share/doc/virtualmoon/changelog $destdir/share/doc/virtualmoon/changelog
-install -v -m 644 Installer/Linux/vmapro/share/doc/virtualmoon/copyright $destdir/share/doc/virtualmoon/copyright
-install -v -m 644 Installer/Linux/vmapro/share/pixmaps/virtualmoon.xpm $destdir/share/pixmaps/virtualmoon.xpm
 install -v -m 644 data/country.tab $destdir/share/virtualmoon/data/
 install -v -m 644 data/retic.cur $destdir/share/virtualmoon/data/
 install -v -m 644 data/jpleph/unxp1900.421 $destdir/share/virtualmoon/data/jpleph/
