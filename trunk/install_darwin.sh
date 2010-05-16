399f3a43c612deb00aec9162bfe4b1360431c576
pchev <pchev@c546a64b-cd0d-0410-a96a-e873a23ec1ac>
2010-05-16 15:22:21 +0000

    install scipts for darwin

--- trunk/install_darwin.sh
+++ trunk/install_darwin.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+# install the software
+
 destdir=$1
 
 if [ -z "$destdir" ]; then
@@ -8,6 +10,7 @@ fi
 
 echo Install virtualmoon to $destdir
 
+# software
 install -m 755 -d $destdir
 install -m 755 -d $destdir/atlun.app
 install -m 755 -d $destdir/atlun.app/Contents
@@ -38,20 +41,8 @@ install -v -m 755 virtualmoon/library/plan404/libplan404.dylib  $destdir/libplan
 install -v -m 644 Installer/Mac/vmapro/licence.txt $destdir/
 install -v -m 644 Installer/Mac/vmapro/readme.txt $destdir/
 
-install -m 755 -d $destdir/data
-install -m 755 -d $destdir/data/jpleph
+# translation
 install -m 755 -d $destdir/language
-install -m 755 -d "$destdir/My Images"
-install -m 755 -d $destdir/Database
-install -m 755 -d $destdir/doc
-install -m 755 -d $destdir/Encyclopedia
-install -m 755 -d $destdir/Textures
-install -m 755 -d $destdir/Textures/Airbrush
-install -m 755 -d $destdir/Textures/Airbrush/L1
-install -m 755 -d $destdir/Textures/Bumpmap
-install -m 755 -d $destdir/Textures/Overlay
-install -m 755 -d $destdir/Textures/Overlay/caption
-
 install -v -m 644 virtualmoon/language/maplun.en.po $destdir/language/
 install -v -m 644 virtualmoon/language/maplun.fr.po $destdir/language/
 install -v -m 644 datlun/language/datlun.en.po $destdir/language/
@@ -60,14 +51,15 @@ install -v -m 644 datlun/language/vmadatabase.en.po $destdir/language/
 install -v -m 644 datlun/language/vmadatabase.fr.po $destdir/language/
 install -v -m 644 photlun/language/photlun.en.po $destdir/language/
 install -v -m 644 photlun/language/photlun.fr.po $destdir/language/
+
+install -m 755 -d $destdir/data
 install -v -m 644 data/country.tab $destdir/data/
 install -v -m 644 data/retic.cur $destdir/data/
-install -v -m 644 data/jpleph/unxp1900.421 $destdir/data/jpleph/
-install -v -m 644 textures/Airbrush/L1/* $destdir/Textures/Airbrush/L1/
-install -v -m 644 textures/Bumpmap/*.jpg $destdir/Textures/Bumpmap/
-install -v -m 644 textures/Bumpmap/kaguya.txt $destdir/Textures/Bumpmap/
-install -v -m 644 textures/Overlay/*.jpg $destdir/Textures/Overlay/
-install -v -m 644 textures/Overlay/caption/*.jpg $destdir/Textures/Overlay/caption/
-install -v -m 644 Database/* $destdir/Database/
+
+# documentation
+install -m 755 -d "$destdir/My Images"
+install -m 755 -d $destdir/doc
+install -m 755 -d $destdir/Encyclopedia
 install -v -m 644 doc/* $destdir/doc/
 install -v -m 644 Encyclopedia/* $destdir/Encyclopedia/
+
---
+++ trunk/install_darwin_data.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+
+# install minimal data for the Pro version
+# must be installed after the software part
+
+function InstData {
+  pkg=$1.tgz
+  ddir=$2
+  tmpdir=$(mktemp -d -t tmp)
+  pkgz=BaseData/$pkg
+  if [ ! -e $pkgz ]; then
+     curl -L -o $pkgz http://sourceforge.net/projects/virtualmoon/files/6-Source_Data/$pkg/download
+  fi
+  tar xvzf $pkgz -C $tmpdir
+  cp -R -p $tmpdir/share/virtualmoon/* $ddir/
+  rm -rf $tmpdir/share/virtualmoon/*
+  rmdir $tmpdir/share/virtualmoon
+  rmdir $tmpdir/share
+  rmdir $tmpdir
+}
+
+destdir=$1
+
+if [ -z "$destdir" ]; then
+   export destdir=/tmp/virtualmoon
+fi
+
+echo Install virtualmoon data to $destdir
+
+install -m 755 -d $destdir/Database
+install -v -m 644 Database/* $destdir/Database/
+
+InstData Base_JPLeph $destdir
+InstData Base_Airbrush $destdir
+InstData Base_Bumpmap $destdir
+InstData Base_Clementine $destdir
+InstData Base_Overlay $destdir
+
