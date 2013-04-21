41c42df430a54abab3fab5430a383160c451a39e
pchev <pchev@c546a64b-cd0d-0410-a96a-e873a23ec1ac>
2013-04-21 09:53:46 +0000

    add datlun to update

--- trunk/install_win_update.sh
+++ trunk/install_win_update.sh
@@ -68,9 +68,11 @@ install -m 755 -d $destdir
 
 if [ $OS_TARGET = win32 ]; then
   i386-win32-strip -v -o $destdir/atlun.exe virtualmoon/atlun.exe 
+  i386-win32-strip -v -o $destdir/atlun.exe virtualmoon/datlun.exe 
 fi
 if [ $OS_TARGET = win64 ]; then
   x86_64-win64-strip -v -o $destdir/atlun.exe virtualmoon/atlun.exe 
+  x86_64-win64-strip -v -o $destdir/atlun.exe virtualmoon/datlun.exe 
 fi
 install -v -m 644 Installer/Windows/Data/readme.txt $destdir/
 install -v -m 644 Installer/Windows/Data/lisezmoi.txt $destdir/
