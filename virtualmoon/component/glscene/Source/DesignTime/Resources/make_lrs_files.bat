e9ef3898a73e10ec290e4485b65c017b5bd91e51
Patrick Chevalley <pch@ap-i.net>
2014-02-10 12:26:12 +0000

    Update GLScene

--- virtualmoon/component/glscene/Source/DesignTime/Resources/make_lrs_files.bat
+++
@@ -1,7 +0,0 @@
-@echo off
-del *.lrs
-lazres.exe GLSceneObjectsLCL.lrs @GLSceneObjectsLCL.rc
-lazres.exe GLSceneLCL.lrs @GLSceneLCL.rc
-lazres.exe nonGLSceneLCL.lrs @nonGLSceneLCL.rc
-lazres.exe GLSceneRunTimeLCL.lrs @GLSceneRunTimeLCL.rc
-pause
