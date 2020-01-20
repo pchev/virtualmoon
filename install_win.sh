c91e715abb142f4811a64158a9c0965c25ce101f
Patrick Chevalley <pch@ap-i.net>
2020-01-20 14:27:06 +0100

    remove old plugin

--- install_win.sh
+++ install_win.sh
@@ -25,7 +25,6 @@ if [ $OS_TARGET = win32 ]; then
   i386-win32-strip -v -o $destdir/cclun.exe cclun/cclun.exe
   install -v -m 644 virtualmoon/library/plan404/libplan404.dll  $destdir/
   unzip -d $destdir Installer/Windows/Data/sqlite3.zip
-  unzip -d $destdir Installer/Windows/Data/plugins.zip
 fi
 if [ $OS_TARGET = win64 ]; then
   x86_64-win64-strip -v -o $destdir/atlun.exe virtualmoon/atlun.exe 
