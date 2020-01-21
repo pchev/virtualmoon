a2bbeeef58d30567263f8a792843b2ffedb338b4
Patrick Chevalley <pch@ap-i.net>
2020-01-21 15:59:40 +0100

    Update translation

--- datlun/makepo.sh
+++ datlun/makepo.sh
@@ -2,7 +2,7 @@
 # resource string in u_translation.pas and compilation of the program
 # Update first the path to your Lazarus installation and run "make" in lazarus/tools
 
-rstconv -i units/x86_64-linux-gtk2/u_translation.rst -o language/datlun.po
-rstconv -i units/x86_64-linux-gtk2/u_translation_database.rst -o language/vmadatabase.po
+rstconv -i units/x86_64-linux-gtk2/u_translation.rsj -o language/datlun.po
+rstconv -i units/x86_64-linux-gtk2/u_translation_database.rsj -o language/vmadatabase.po
 /home/compiler/lazarus/tools/updatepofiles language/datlun.po
 /home/compiler/lazarus/tools/updatepofiles language/vmadatabase.po
--- photlun/makepo.sh
+++ photlun/makepo.sh
@@ -2,5 +2,5 @@
 # resource string in u_translation.pas and compilation of the program
 # Update first the path to your Lazarus installation and run "make" in lazarus/tools
 
-rstconv -i units/x86_64-linux-gtk2/u_translation.rst -o language/photlun.po
+rstconv -i units/x86_64-linux-gtk2/u_translation.rsj -o language/photlun.po
 /home/compiler/lazarus/tools/updatepofiles language/photlun.po
