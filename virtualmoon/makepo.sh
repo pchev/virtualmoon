90a4736b7ddf3386e5d318f374513844621ce743
Patrick Chevalley <pch@ap-i.net>
2022-07-20 17:22:22 +0200

    Update translation

--- virtualmoon/makepo.sh
+++ virtualmoon/makepo.sh
@@ -2,5 +2,5 @@
 # resource string in u_translation.pas and compilation of the program
 # Update first the path to your Lazarus installation and run "make" in lazarus/tools
 
-rstconv -i units/x86_64-linux-gtk2/u_translation.rsj -o language/maplun.po
-/home/compiler/lazarus/tools/updatepofiles language/maplun.po
+rstconv -i units/x86_64-linux-gtk2/u_translation.rsj -o language/maplun.pot
+/home/compiler/lazarus/tools/updatepofiles language/maplun.pot
