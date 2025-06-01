3470025ee088f8399123784dc8bdd27ec7252a84
Patrick Chevalley <pch@ap-i.net>
2025-06-01 11:20:30 +0200

    Update translation

--- makepo.sh
+++ makepo.sh
@@ -1,23 +1,30 @@
 #!/bin/bash
 # Update the translations in all modules
 cd calclun
+pwd
 ./makepo.sh
-cd -
+cd - >/dev/null
 cd cclun
+pwd
 ./makepo.sh
-cd -
+cd - >/dev/null
 cd datlun
+pwd
 ./makepo.sh
-cd -
+cd - >/dev/null
 cd notelun
+pwd
 ./makepo.sh
-cd -
+cd - >/dev/null
 cd photlun
+pwd
 ./makepo.sh
-cd -
+cd - >/dev/null
 cd virtualmoon
+pwd
 ./makepo.sh
-cd -
+cd - >/dev/null
 cd weblun
+pwd
 ./makepo.sh
-cd -
+cd - >/dev/null
