f0ee283f37292414752b5046144897113a96579d
Patrick Chevalley <pch@ap-i.net>
2026-07-15 09:12:49 +0200

    Set version 9.1

--- virtualmoon/u_constant.pas
+++ virtualmoon/u_constant.pas
@@ -33,9 +33,9 @@ uses
 
 const crlf = chr(10)+chr(13);
       cpyr = '©'; //chr($a9)+chr($c2);  // ©
-      AVLversion = '9.0';
-      version = '9.0a';
-      avlcpy = 'Copyright '+cpyr+' 2002-2025 Christian Legrand, Patrick Chevalley';
+      AVLversion = '9.1';
+      version = '9.1a';
+      avlcpy = 'Copyright '+cpyr+' 2002-2026 Christian Legrand, Patrick Chevalley';
       vmaurl='http://ap-i.net/avl';
       jd2000 =2451545.0 ;
       jd1950 =2433282.4235;
